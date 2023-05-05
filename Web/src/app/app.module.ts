c2e07d2f6987e6af6073d9e28a6b19d1f424d656
Bogdan Hardau <alboghardau@gmail.com>
2023-05-05 23:50:57 +0300

    small add to web

--- Web/src/app/app.module.ts
+++ Web/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
+import { OptionsBarComponent } from './components/options-bar/options-bar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, OptionsBarComponent],
   imports: [BrowserModule, ButtonModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ Web/src/app/components/options-bar/options-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'options-bar',
+  templateUrl: './options-bar.component.html',
+  styleUrls: ['./options-bar.component.scss'],
+})
+export class OptionsBarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
