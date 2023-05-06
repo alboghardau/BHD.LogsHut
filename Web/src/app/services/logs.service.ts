c960926e077261bcc108bbde60228c1dc95d0839
Bogdan Hardau <alboghardau@gmail.com>
2023-05-07 00:30:07 +0300

    add services

---
+++ Web/src/app/services/logs.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Log } from '../models/Log';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LogsService {
+  private logs: Log[] = [];
+
+  constructor() {}
+
+  public getAllLogs(): Log[] {
+    return this.logs;
+  }
+
+  public addLog(log: Log): void {
+    this.logs.push(log);
+  }
+
+  public clearLogs(): void {
+    this.logs = [];
+  }
+}
