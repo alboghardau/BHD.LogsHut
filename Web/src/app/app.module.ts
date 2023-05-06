54f5065c23801b73de36af8c66ea4fd513c3dbf7
Bogdan Hardau <alboghardau@gmail.com>
2023-05-06 23:55:30 +0300

    small ui update

--- Web/src/app/app.module.ts
+++ Web/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 import { OptionsBarComponent } from './components/options-bar/options-bar.component';
+import { LogsTableComponent } from './components/logs-table/logs-table.component';
 
 @NgModule({
-  declarations: [AppComponent, OptionsBarComponent],
-  imports: [BrowserModule, ButtonModule],
+  declarations: [AppComponent, OptionsBarComponent, LogsTableComponent],
+  imports: [BrowserModule, ButtonModule, TableModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ Web/src/app/components/logs-table/logs-table.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'logs-table',
+  templateUrl: './logs-table.component.html',
+  styleUrls: ['./logs-table.component.scss'],
+})
+export class LogsTableComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
