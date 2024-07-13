1c266c5fbc4b76e493beeb8d2d42305489318ccd
Bogdan Hardau <alboghardau@gmail.com>
2024-07-13 22:23:38 +0300

    Clean old files in repo

--- Service/BHD.Logger/DTOs/NewLogsRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace BHD.Logger.DTOs
-{
-	public class NewLogsRequestDto
-	{
-        public DateTime Time { get; set; }  
-	}
-}
-
--- Web/src/app/enums/log-level.ts
+++
@@ -1,7 +0,0 @@
-export enum LogLevel {
-  Verbose,
-  Trace,
-  Info,
-  Warning,
-  Error,
-}
